fa5458bcd70a76146112fc743a075b7bc356236f
Dmitriy Beseda <BesedaDG@gmail.com>
2016-07-24 20:50:20 +0300

    Added directive example

---
+++ directive-example/myDirective.js
@@ -0,0 +1,28 @@
+(function() {
+    angular
+        .module('directiveExample', [])
+        .directive('myButton', myButton);
+
+    myButton.$inject = [
+        '$window'
+    ];
+
+    function myButton($window) {
+        return {
+            restrict: 'E',
+            scope: {
+                href: '=href'
+            },
+            transclude: true,
+            // replace: true,
+            link: function(scope, element, attrs) {
+                element.bind('click', function(event) {
+                    console.log(attrs);
+                    // $window.open(scope.href);
+                    scope.href = '123';
+                });
+            },
+            template: '<button ng-transclude></button>'
+        };
+    }
+})();
